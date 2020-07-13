72d6247b9fa9841fa2b61d9c220c74cbc061f74f
Gavin Quirk <gavinquirk@gmail.com>
2020-07-13 15:14:21 -0700

    basic hamburger nav

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 console.log('JavaScript loaded...');
+
+const wrapper = document.getElementById('hamburger-wrapper');
+const hamburgerButton = document.getElementsByClassName('hamburger-button');
+const navOverlay = document.getElementsByClassName('nav-overlay');
+
+// When menu button is clicked
+wrapper.onclick = () => {
+  // Toggle closed class on hamburger
+  hamburgerButton[0].classList.toggle('hamburger-closed');
+  // Toggle open overlay
+  navOverlay[0].classList.toggle('nav-open');
+};
